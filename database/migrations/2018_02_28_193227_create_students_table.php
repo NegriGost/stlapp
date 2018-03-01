3902eeb744e189288c9fa77814f75155ef163306
NegriGost <mafumorodrigues@gmail.com>
2018-03-01 22:35:42 +0200

    Adicionando Atributos a Tabela students

--- database/migrations/2018_02_28_193227_create_students_table.php
+++ database/migrations/2018_02_28_193227_create_students_table.php
@@ -14,8 +14,28 @@ class CreateStudentsTable extends Migration
     public function up()
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->increments('student_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('telephone');
+            $table->integer('city_id')->unsigned();
+            $table->integer('school_id')->unsigned();
+            $table->integer('submission_id')->unsigned();
+            $table->text('digital_tools');
+            $table->binary('workplace_tools');
+            $table->text('workplace_tools_othe');
+            $table->integer('teaching_grade')->unsigned();
+            $table->text('work_methods');
+            $table->text('subjects');
+            $table->integer('years_as_teacher')->unsigned();
+            $table->tinyInteger('technical_support');
+            $table->tinyInteger('student_to_student_feedback');
+            $table->text('student_to_student_feedback_other');
+            $table->text('technology_use_in_teaching');
+            $table->foreign('city_id')->references('city_id')->on('cities') ->onDelete('cascade'); 
+            $table->foreign('school_id')->references('school_id')->on('schools') ->onDelete('cascade'); 
+            $table->foreign('submission_id')->references('submission_id')->on('assignment_submission') ->onDelete('cascade'); 
+            
         });
     }
 
