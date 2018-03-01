ac4bf12bbe500ea591c44fde9f535967ac5b53c5
NegriGost <mafumorodrigues@gmail.com>
2018-03-01 22:18:43 +0200

    Adicionando Atributos a Tabela assignment_descriptions

--- database/migrations/2018_02_28_193148_create_assignment_descriptions_table.php
+++ database/migrations/2018_02_28_193148_create_assignment_descriptions_table.php
@@ -14,8 +14,16 @@ class CreateAssignmentDescriptionsTable extends Migration
     public function up()
     {
         Schema::create('assignment_descriptions', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->increments('assignment_id');
+            $table->text('case');
+            $table->text('instructions');
+            $table->date('startdate');
+            $table->date('deadline');
+            $table->date('available_date');
+            $table->integer('teacher_id')->unsigned();
+            $table->integer('course_id')->unsigned();
+            $table->foreign('teacher_id')->references('teacher_id')->on('teachers') ->onDelete('cascade'); 
+            $table->foreign('course_id')->references('course_id')->on('courses') ->onDelete('cascade'); 
         });
     }
 
