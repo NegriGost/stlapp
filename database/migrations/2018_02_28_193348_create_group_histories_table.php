a1ba9fe8b33f1faa539dd072dbfac0c6c13439b1
NegriGost <mafumorodrigues@gmail.com>
2018-03-02 00:08:51 +0200

    Adicionando Atributos a Tabela group_history

--- database/migrations/2018_02_28_193348_create_group_histories_table.php
+++ database/migrations/2018_02_28_193348_create_group_histories_table.php
@@ -14,8 +14,15 @@ class CreateGroupHistoriesTable extends Migration
     public function up()
     {
         Schema::create('group_histories', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->increments('history_id');
+            $table->integer('student_id')->unsigned();
+            $table->integer('group_id')->unsigned();
+            $table->integer('assignment_id')->unsigned();
+            $table->date('deadline');
+            $table->foreign('student_id')->references('student_id')->on('students') ->onDelete('cascade'); 
+            $table->foreign('group_id')->references('group_id')->on('group_assignments') ->onDelete('cascade'); 
+            $table->foreign('assignment_id')->references('assignment_id')->on('assignment_descriptions') ->onDelete('cascade'); 
+            
         });
     }
 
